7081e27b1d4c5677ce1f04e25bc84e098406a5e3
Taiwo Adejare <adejareemma@gmail.com>
2022-02-17 23:58:00 +0100

    added not found api

--- src/gateway/api.js
+++ src/gateway/api.js
@@ -6,11 +6,14 @@ const swaggerUi = require('swagger-ui-express');
 const UserMicroService = require('../users');
 const PostMicroService = require('../posts');
 const { isLoggedIn } = require('./middlewares');
+const { Response } = require('../utils');
 
 // API Documentation
 const docs = yaml.load(`${__dirname}/docs/swagger.yaml`);
 apis.use('/docs', swaggerUi.serve, swaggerUi.setup(docs));
 apis.use('/users', UserMicroService.router);
 apis.use('/posts', isLoggedIn, PostMicroService.router);
+
+apis.use('*', (req, res) => Response.error(res, 404, 'Not Found'));
 
 module.exports = { apis };
