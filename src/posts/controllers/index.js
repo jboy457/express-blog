b1bf1204f9383ae7f61360250e353e00d70c19c3
Taiwo Adejare <adejareemma@gmail.com>
2022-02-17 14:18:56 +0100

    export post controller

---
+++ src/posts/controllers/index.js
@@ -0,0 +1,7 @@
+const {
+  createPost, getAllPost, getPost, deleteUserPost, updateUserPost
+} = require('./post_controller');
+
+module.exports = Object.freeze({
+  createPost, getAllPost, getPost, deleteUserPost, updateUserPost
+});
