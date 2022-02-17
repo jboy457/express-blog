0ba9b95243ec08eabf03eed2ced76fcf196e9ebd
Taiwo Adejare <adejareemma@gmail.com>
2022-02-17 14:25:55 +0100

    update middleware for register user

--- src/users/user.js
+++ src/users/user.js
@@ -4,7 +4,7 @@ const { Validate } = require('../utils');
 const { register, authenticate } = require('./controllers');
 const { CreateUserSchema, AuthenicateSchema } = require('./validations');
 
-router.post('/register', Validate(CreateUserSchema, true), register);
+router.post('/register', Validate(CreateUserSchema), register);
 router.post('/login', Validate(AuthenicateSchema), authenticate);
 
 module.exports = { router };
