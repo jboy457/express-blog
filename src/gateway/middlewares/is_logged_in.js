33f3a8d5599719e20608970dec83de95c3620376
Taiwo Adejare <adejareemma@gmail.com>
2022-02-17 14:17:29 +0100

    is logged in middleware

---
+++ src/gateway/middlewares/is_logged_in.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-underscore-dangle */
+const { UserRepository } = require('../../users/repositories');
+const { Response, JWT } = require('../../utils');
+
+module.exports = Object.freeze({
+  isLoggedIn: async (req, res, next) => {
+    try {
+      if (!req.header('x-auth-token')) return Response.error(res, 401, 'Unauthorized user. Log in and try again');
+      const decoded = await JWT.verify(req.header('x-auth-token'));
+      if (!decoded._id) return Response.error(res, 401, 'Session Expired');
+      const user = await UserRepository.findByEmail(decoded.email);
+      if (!user) return Response.error(res, 401, 'Unauthorized user. Log in and try again');
+      req.user = user;
+      req.token = req.header('x-auth-token');
+      return next();
+    } catch (err) {
+      return Response.error(res, 500, err.message);
+    }
+  }
+});
