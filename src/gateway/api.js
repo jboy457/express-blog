7f38e7bbb38a1105929d5b51ff93ec747c17a4f8
Taiwo Adejare <adejareemma@gmail.com>
2022-02-17 14:17:05 +0100

    added post micro service to gateway

--- src/gateway/api.js
+++ src/gateway/api.js
@@ -1,6 +1,9 @@
 const apis = require('express').Router();
 const UserMicroService = require('../users');
+const PostMicroService = require('../posts');
+const { isLoggedIn } = require('./middlewares');
 
 apis.use('/user', UserMicroService.router);
+apis.use('/post', isLoggedIn, PostMicroService.router);
 
 module.exports = { apis };
