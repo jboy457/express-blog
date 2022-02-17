46a56c04f414630e178523136ca0ce5937b901fe
Taiwo Adejare <adejareemma@gmail.com>
2022-02-17 02:23:35 +0100

    init app.js

---
+++ src/app.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+require('./config/database').connect();
+
+const compression = require('compression');
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+
+// const routes = require('./routes');
+
+const app = express();
+
+app.use(cors());
+app.use(compression());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+// app.use(routes);
+
+module.exports = app;
