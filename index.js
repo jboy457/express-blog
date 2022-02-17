de51acbf1f29b74cea18266c03af4dbad2d5479c
Taiwo Adejare <adejareemma@gmail.com>
2022-02-17 02:21:25 +0100

    created server index file

---
+++ index.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-console */
+const app = require('./src/app');
+
+const startServer = async () => {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, (err) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(`Bot listenenig on port ${PORT}`);
+  });
+};
+
+startServer();
