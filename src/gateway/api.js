c128852afce1f339efd10fd413b1be093b226e1d
Taiwo Adejare <adejareemma@gmail.com>
2022-02-17 16:18:18 +0100

    configured swagger into app routes

--- src/gateway/api.js
+++ src/gateway/api.js
@@ -1,8 +1,15 @@
 const apis = require('express').Router();
+
+const yaml = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
+
 const UserMicroService = require('../users');
 const PostMicroService = require('../posts');
 const { isLoggedIn } = require('./middlewares');
 
+// API Documentation
+const docs = yaml.load(`${__dirname}/docs/swagger.yaml`);
+apis.use('/docs', swaggerUi.serve, swaggerUi.setup(docs));
 apis.use('/user', UserMicroService.router);
 apis.use('/post', isLoggedIn, PostMicroService.router);
 
