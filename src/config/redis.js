ecdf2259cae51614a30d0211afb3d5d3a1904a94
Taiwo Adejare <adejareemma@gmail.com>
2022-02-18 02:25:20 +0100

    redis config file

---
+++ src/config/redis.js
@@ -0,0 +1,16 @@
+const redis = require('redis');
+const logger = require('./logger');
+
+const redisClient = redis.createClient();
+
+redisClient.connect();
+
+redisClient.on('connect', () => {
+  logger.info('Successfully connected to redis!!');
+});
+
+redisClient.on('error', (error) => {
+  logger.error(error);
+});
+
+module.exports = redisClient;
