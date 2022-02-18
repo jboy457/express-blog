a356b7d028ff833c566e23d163f6bae6c99feefd
Taiwo Adejare <adejareemma@gmail.com>
2022-02-18 02:27:54 +0100

    cache user for islogged in middleware

--- src/gateway/middlewares/is_logged_in.js
+++ src/gateway/middlewares/is_logged_in.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-underscore-dangle */
+const { redisClient } = require('../../config');
 const { UserRepository } = require('../../users/repositories');
 const { Response, JWT } = require('../../utils');
 
@@ -8,7 +9,15 @@ module.exports = Object.freeze({
       if (!req.header('x-auth-token')) return Response.error(res, 401, 'Unauthorized user. Log in and try again');
       const decoded = await JWT.verify(req.header('x-auth-token'));
       if (!decoded._id) return Response.error(res, 401, 'Session Expired');
-      const user = await UserRepository.findByEmail(decoded.email);
+
+      let user;
+      const cacheUser = await redisClient.get(decoded._id);
+      user = JSON.parse(cacheUser);
+      if (!user) {
+        user = await UserRepository.findByEmail(decoded.email);
+        redisClient.set(user._id, JSON.stringify(user));
+      }
+
       if (!user) return Response.error(res, 401, 'Unauthorized user. Log in and try again');
       req.user = user;
       req.token = req.header('x-auth-token');
